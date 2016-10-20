fc718636c8337f50530e6e6220d974092c6c36af
Thomas Pothier Jr <tjpothier17@gmail.com>
2016-10-20 06:04:40 -0500

    10/20/2016; 6:04 am

--- java-track/assignments/Student.java
+++ java-track/assignments/Student.java
@@ -1,8 +1,35 @@
 
 public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private int studentID;
+	private int credits = 0;
+	private double GPA = 0;
+	
+	public Student(String firstName, String lastName, int studentID) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.studentID = studentID;
+	}
+	
+	public String getName() {
+		return this.firstName + " " + this.lastName;
+	}
+	
+	public int getStudentID() {
+		return this.studentID;
+	}
+	
+	public int getCredits() {
+		return this.credits;
+	}
 
+	public double getGPA() {
+		return this.GPA;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
